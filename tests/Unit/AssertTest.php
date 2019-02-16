10614ef41492aad7340d62520e556e049891ab7e
Rafael Dohms <rdohms@gmail.com>
2019-02-16 23:24:25 +0100

    Import current implementation as-is
    Copy over from PHPunit the code that will be deprecated, adjusting only
    the needed tests, some coding style fixes and the namespaces. More
    refactoring and revisiting to follow.

---
+++ tests/Unit/AssertTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace DMS\PHPUnitExtensions\ArraySubset\Tests\Unit;
+
+use DMS\PHPUnitExtensions\ArraySubset\Assert;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Exception;
+
+final class AssertTest extends TestCase
+{
+    public function testAssertArraySubsetPassesStrictConfig(): void
+    {
+        $this->expectException(ExpectationFailedException::class);
+        Assert::assertArraySubset(['bar' => 0], ['bar' => '0'], true);
+    }
+
+    public function testAssertArraySubsetThrowsExceptionForInvalidSubset(): void
+    {
+        $this->expectException(ExpectationFailedException::class);
+        Assert::assertArraySubset([6,7], [1,2,3,4,5,6]);
+    }
+
+    public function testAssertArraySubsetThrowsExceptionForInvalidSubsetArgument(): void
+    {
+        $this->expectException(Exception::class);
+        Assert::assertArraySubset('string', '');
+    }
+
+    public function testAssertArraySubsetThrowsExceptionForInvalidArrayArgument(): void
+    {
+        $this->expectException(Exception::class);
+        Assert::assertArraySubset([], '');
+    }
+
+    public function testAssertArraySubsetDoesNothingForValidScenario(): void
+    {
+        Assert::assertArraySubset([1,2], [1,2,3]);
+    }
+}
---
+++ tests/Unit/Constraint/ArraySubsetTest.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+
+namespace DMS\PHPUnitExtensions\ArraySubset\Tests\Unit\Constraint;
+
+use ArrayAccessible;
+use ArrayObject;
+use Countable;
+use DMS\PHPUnitExtensions\ArraySubset\Constraint\ArraySubset;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\SelfDescribing;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use Traversable;
+use function sprintf;
+
+final class ArraySubsetTest extends TestCase
+{
+    public static function evaluateDataProvider(): array
+    {
+        return [
+            'loose array subset and array other' => [
+                'expected' => true,
+                'subset'   => ['bar' => 0],
+                'other'    => ['foo' => '', 'bar' => '0'],
+                'strict'   => false,
+            ],
+            'strict array subset and array other' => [
+                'expected' => false,
+                'subset'   => ['bar' => 0],
+                'other'    => ['foo' => '', 'bar' => '0'],
+                'strict'   => true,
+            ],
+            'loose array subset and ArrayObject other' => [
+                'expected' => true,
+                'subset'   => ['bar' => 0],
+                'other'    => new ArrayObject(['foo' => '', 'bar' => '0']),
+                'strict'   => false,
+            ],
+            'strict ArrayObject subset and array other' => [
+                'expected' => true,
+                'subset'   => new ArrayObject(['bar' => 0]),
+                'other'    => ['foo' => '', 'bar' => 0],
+                'strict'   => true,
+            ],
+        ];
+    }
+
+    /**
+     * @param array|Traversable $subset
+     * @param array|Traversable $other
+     * @param bool              $strict
+     *
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     * @dataProvider evaluateDataProvider
+     */
+    public function testEvaluate(bool $expected, $subset, $other, $strict): void
+    {
+        $constraint = new ArraySubset($subset, $strict);
+
+        $this->assertSame($expected, $constraint->evaluate($other, '', true));
+    }
+
+    public function testEvaluateWithArrayAccess(): void
+    {
+        $arrayAccess = new ArrayAccessible(['foo' => 'bar']);
+
+        $constraint = new ArraySubset(['foo' => 'bar']);
+
+        $this->assertTrue($constraint->evaluate($arrayAccess, '', true));
+    }
+
+    public function testEvaluateFailMessage(): void
+    {
+        $constraint = new ArraySubset(['foo' => 'bar']);
+
+        try {
+            $constraint->evaluate(['baz' => 'bar'], '', false);
+            $this->fail(sprintf('Expected %s to be thrown.', ExpectationFailedException::class));
+        } catch (ExpectationFailedException $expectedException) {
+            $comparisonFailure = $expectedException->getComparisonFailure();
+            $this->assertNotNull($comparisonFailure);
+            $this->assertStringContainsString("'foo' => 'bar'", $comparisonFailure->getExpectedAsString());
+            $this->assertStringContainsString("'baz' => 'bar'", $comparisonFailure->getActualAsString());
+        }
+    }
+
+    public function testIsCountable(): void
+    {
+        $reflection = new ReflectionClass(ArraySubset::class);
+
+        $this->assertTrue($reflection->implementsInterface(Countable::class), sprintf(
+            'Failed to assert that ArraySubset implements "%s".',
+            Countable::class
+        ));
+    }
+
+    public function testIsSelfDescribing(): void
+    {
+        $reflection = new ReflectionClass(ArraySubset::class);
+
+        $this->assertTrue($reflection->implementsInterface(SelfDescribing::class), sprintf(
+            'Failed to assert that Array implements "%s".',
+            SelfDescribing::class
+        ));
+    }
+}

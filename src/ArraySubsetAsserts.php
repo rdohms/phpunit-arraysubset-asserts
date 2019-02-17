dca69cccb9d5d43b94c128ff5a96a87457156977
Rafael Dohms <rdohms@gmail.com>
2019-02-17 14:29:27 +0100

    Move asserts to trait
    Offering a trait allows users to easily hook this class into their own
    test cases with ease, as an option of usage.

---
+++ src/ArraySubsetAsserts.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace DMS\PHPUnitExtensions\ArraySubset;
+
+use ArrayAccess;
+use DMS\PHPUnitExtensions\ArraySubset\Constraint\ArraySubset;
+use Exception;
+use function is_array;
+use PHPUnit\Framework\Assert as PhpUnitAssert;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Util\InvalidArgumentHelper;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+trait ArraySubsetAsserts
+{
+    /**
+     * Asserts that an array has a specified subset.
+     *
+     * @param array|ArrayAccess $subset
+     * @param array|ArrayAccess $array
+     *
+     * @throws ExpectationFailedException
+     * @throws InvalidArgumentException
+     * @throws Exception
+     */
+    public static function assertArraySubset($subset, $array, bool $checkForObjectIdentity = false, string $message = ''): void
+    {
+        if (! (is_array($subset) || $subset instanceof ArrayAccess)) {
+            throw InvalidArgumentHelper::factory(
+                1,
+                'array or ArrayAccess'
+            );
+        }
+        if (! (is_array($array) || $array instanceof ArrayAccess)) {
+            throw InvalidArgumentHelper::factory(
+                2,
+                'array or ArrayAccess'
+            );
+        }
+        $constraint = new ArraySubset($subset, $checkForObjectIdentity);
+        PhpUnitAssert::assertThat($array, $constraint, $message);
+    }
+}

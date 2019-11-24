f0c3eff775fe8e983a6d1226866c24ddb8e1e2f8
Brian Stanley <bmstanley87@gmail.com>
2019-11-24 13:46:50 -0500

    Fix class PHPUnit\Util\InvalidArgumentHelper not found error in PHPUnit 8.4

--- src/ArraySubsetAsserts.php
+++ src/ArraySubsetAsserts.php
@@ -8,8 +8,7 @@ use DMS\PHPUnitExtensions\ArraySubset\Constraint\ArraySubset;
 use Exception;
 use PHPUnit\Framework\Assert as PhpUnitAssert;
 use PHPUnit\Framework\ExpectationFailedException;
-use PHPUnit\Util\InvalidArgumentHelper;
-use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use PHPUnit\Framework\InvalidArgumentException;
 use function is_array;
 
 trait ArraySubsetAsserts
@@ -19,6 +18,8 @@ trait ArraySubsetAsserts
      *
      * @param array|ArrayAccess|mixed[] $subset
      * @param array|ArrayAccess|mixed[] $array
+     * @param bool                      $checkForObjectIdentity
+     * @param string                    $message
      *
      * @throws ExpectationFailedException
      * @throws InvalidArgumentException
@@ -27,13 +28,13 @@ trait ArraySubsetAsserts
     public static function assertArraySubset($subset, $array, bool $checkForObjectIdentity = false, string $message = ''): void
     {
         if (! (is_array($subset) || $subset instanceof ArrayAccess)) {
-            throw InvalidArgumentHelper::factory(
+            throw InvalidArgumentException::create(
                 1,
                 'array or ArrayAccess'
             );
         }
         if (! (is_array($array) || $array instanceof ArrayAccess)) {
-            throw InvalidArgumentHelper::factory(
+            throw InvalidArgumentException::create(
                 2,
                 'array or ArrayAccess'
             );

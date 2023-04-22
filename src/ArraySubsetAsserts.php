815b4a6a887af8e27440576a5f2e8ae4f7d89faf
Rafael Dohms <rdohms@gmail.com>
2023-04-22 23:27:38 +0200

    Add Support for PHPUnit 10
    We no longer rely on the internal InvalidArgumentException from PHPUnit,
    but we still keep previous behavior for polyfill reasons.
    
    Co-authored-by: Benedikt Franke <benedikt.franke@mll.com>

--- src/ArraySubsetAsserts.php
+++ src/ArraySubsetAsserts.php
@@ -7,13 +7,15 @@ namespace DMS\PHPUnitExtensions\ArraySubset;
 use ArrayAccess;
 use DMS\PHPUnitExtensions\ArraySubset\Constraint\ArraySubset;
 use Exception;
+use InvalidArgumentException;
 use PHPUnit\Framework\Assert as PhpUnitAssert;
 use PHPUnit\Framework\ExpectationFailedException;
-use PHPUnit\Framework\InvalidArgumentException;
+use PHPUnit\Framework\InvalidArgumentException as PHPUnitInvalidArgumentException;
 use PHPUnit\Util\InvalidArgumentHelper;
 
 use function class_exists;
 use function is_array;
+use function method_exists;
 
 trait ArraySubsetAsserts
 {
@@ -24,41 +26,57 @@ trait ArraySubsetAsserts
      * @param array|ArrayAccess|mixed[] $array
      *
      * @throws ExpectationFailedException
-     * @throws InvalidArgumentException|Exception
+     * @throws InvalidArgumentException
      * @throws Exception
      */
     public static function assertArraySubset($subset, $array, bool $checkForObjectIdentity = false, string $message = ''): void
     {
         if (! (is_array($subset) || $subset instanceof ArrayAccess)) {
-            if (class_exists(InvalidArgumentException::class)) {
-                // PHPUnit 8.4.0+.
-                throw InvalidArgumentException::create(
+            if (
+                class_exists(PHPUnitInvalidArgumentException::class)
+                && method_exists(PHPUnitInvalidArgumentException::class, 'create')
+            ) {
+                // PHPUnit 8.4.0+ < 10.0.0.
+                throw PHPUnitInvalidArgumentException::create(
                     1,
                     'array or ArrayAccess'
                 );
             }
 
             // PHPUnit < 8.4.0.
-            throw InvalidArgumentHelper::factory(
-                1,
-                'array or ArrayAccess'
-            );
+            if (class_exists(InvalidArgumentHelper::class)) {
+                throw InvalidArgumentHelper::factory(
+                    1,
+                    'array or ArrayAccess'
+                );
+            }
+
+            // PHPUnit 10+
+            throw new InvalidArgumentException('Argument #1 $subset of assertArraySubset() must be array or ArrayAccess.');
         }
 
         if (! (is_array($array) || $array instanceof ArrayAccess)) {
-            if (class_exists(InvalidArgumentException::class)) {
-                // PHPUnit 8.4.0+.
-                throw InvalidArgumentException::create(
+            if (
+                class_exists(PHPUnitInvalidArgumentException::class)
+                && method_exists(PHPUnitInvalidArgumentException::class, 'create')
+            ) {
+                // PHPUnit 8.4.0+ < 10.0.0.
+                throw PHPUnitInvalidArgumentException::create(
                     2,
                     'array or ArrayAccess'
                 );
             }
 
             // PHPUnit < 8.4.0.
-            throw InvalidArgumentHelper::factory(
-                2,
-                'array or ArrayAccess'
-            );
+            if (class_exists(InvalidArgumentHelper::class)) {
+                throw InvalidArgumentHelper::factory(
+                    2,
+                    'array or ArrayAccess'
+                );
+            }
+
+            // PHPUnit 10+
+            throw new InvalidArgumentException('Argument #2 array of assertArraySubset() must be array or ArrayAccess.');
         }
 
         $constraint = new ArraySubset($subset, $checkForObjectIdentity);
